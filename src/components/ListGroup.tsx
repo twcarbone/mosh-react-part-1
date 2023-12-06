d096a83f3cb660bf0dd45e6ef19a5c49f219e18f
Tyler Carbone <tcarbone073@gmail.com>
2023-12-06 00:04:28 +0000

    Create a ListGroup component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,11 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">Another item</li>
+      <li className="list-group-item">Yet another item</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
