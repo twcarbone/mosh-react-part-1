cf8f28450a939bf180927db014d9984e6b5323e3
Tyler Carbone <tcarbone073@gmail.com>
2023-12-06 00:30:14 +0000

    Wrap ListGroup in Fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,13 @@
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">Another item</li>
-      <li className="list-group-item">Yet another item</li>
-    </ul>
+    <>
+      <h1>List</h1>;
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">Another item</li>
+        <li className="list-group-item">Yet another item</li>
+      </ul>
+    </>
   );
 }
 
