6346f51ac20482aceaa6580c230c7c3cb0ee95f3
Tyler Carbone <tcarbone073@gmail.com>
2023-12-07 10:59:12 +0000

    (fix) Add class name to ListGroup item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,7 +7,9 @@ function ListGroup() {
       <h1>List</h1>;{items.length === 0 && <p>No item found</p>};
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
