665a72555f7f6f3ff0a518be4bc96b5d01d49598
Tyler Carbone <tcarbone073@gmail.com>
2023-12-09 12:47:06 +0000

    Exercise: show dismissible alert (solution)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button onClick={() => console.log("Clicked")}>Click Me!</Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Alert!</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>Click Me!</Button>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,10 +2,23 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div>{children}</div>;
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <>
+      <div className="alert alert-dismissible">
+        {children}
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          onClick={onClose}
+        ></button>
+      </div>
+    </>
+  );
 };
 
 export default Alert;
