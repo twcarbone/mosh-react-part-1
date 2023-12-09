cdfd2cf418e71590bbd6d25c7a555946da832f3e
Tyler Carbone <tcarbone073@gmail.com>
2023-12-09 10:50:49 +0000

    Passing children

---
+++ src/components/Alert.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return <div>{children}</div>;
+};
+
+export default Alert;
