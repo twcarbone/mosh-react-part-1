11177f562731d28d46cc54a74964db7981987469
Tyler Carbone <tcarbone073@gmail.com>
2023-12-11 17:32:18 +0000

    Managing State - Updating arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { useState } from "react";
 
 function App() {
-  const [customer, setCustomer] = useState({
-    name: "John",
-    address: {
-      city: "San Francisco",
-      zipCode: 94111,
-    },
-  });
+  const [tags, setTags] = useState(["happy", "cheerful"]);
 
   function handleClick() {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: 94112 },
-    });
+    // Add
+    setTags([...tags, "exciting"]);
+
+    // Remove
+    setTags(tags.filter((tag) => tag !== "happy"));
+
+    // Update
+    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
   }
 
   return (
     <div>
-      {customer.address.zipCode}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
