f48114b6705866a1841b1554433bdef5cd41a3ba
Tyler Carbone <tcarbone073@gmail.com>
2023-12-15 20:37:57 +0000

    Building Forms - Building ExpenseList - Part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
     </div>
   );
 }
