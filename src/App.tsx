0c61af64d89a2242e5b099ba76596fdefc357cf3
Tyler Carbone <tcarbone073@gmail.com>
2023-12-16 11:04:57 +0000

    Building Forms - Building ExpenseList - Part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 10, category: "Utilities" },
@@ -10,10 +13,19 @@ function App() {
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
 
+  const visibileExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibileExpenses}
         onDelete={(id) =>
           setExpenses(expenses.filter((expense) => expense.id !== id))
         }
---
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+
+function ExpenseFilter({ onSelectCategory }: Props) {
+  return (
+    <select
+      className="form-select"
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
+      <option value="">All Categories</option>
+      <option value="Groceries">Groceries</option>
+      <option value="Utilities">Utilities</option>
+      <option value="Entertainment">Entertainment</option>
+    </select>
+  );
+}
+
+export default ExpenseFilter;
