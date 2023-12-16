2dc1ce84801dcaee9418af9782ea7cbc5c517818
Tyler Carbone <tcarbone073@gmail.com>
2023-12-16 11:32:18 +0000

    Building Forms - Building ExpenseList - Part 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -19,6 +22,9 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
