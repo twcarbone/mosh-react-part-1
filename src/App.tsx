095b7096fcdc564bccbcd4fc6d46df400c45e55b
Tyler Carbone <tcarbone073@gmail.com>
2023-12-17 13:04:05 +0000

    Connecting to Backend - Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const connect = () => console.log("Connecting...");
-const disconnect = () => console.log("Disconnecting...");
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
-    connect();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data));
+  }, []);
 
-    // Returned function is called when effect hook completes.
-    return () => disconnect();
-  });
-
-  return <div></div>;
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
